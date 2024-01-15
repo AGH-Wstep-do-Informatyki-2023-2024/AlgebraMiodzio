7ea830813dc55ccbe8fd9f5297d338db9715b4a3
0Qrb <qrb@student.agh.edu.pl>
2024-01-15 23:21:16 +0100

    cxzc

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from libs.tile import Tile, ApplyRules
 
 DIM = 5
 
-# pg setup rb
+# pg setup r
 pg.init()
 pg.display.set_caption("AlgebraMiodzio - Test")
 screen = pg.display.set_mode((1280, 800))
